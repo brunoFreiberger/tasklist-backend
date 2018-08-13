0800cbbbaa8f27eb88e4bb401399d60a093282bd
brunohfreiberger@gmail.com <brunohfreiberger@gmail.com>
2018-08-13 02:05:09 -0300

    readme and files

--- README.md
+++ README.md
@@ -1,1 +1,46 @@
-# tasklist-backend
+Developement of tasklist using angular 6 with Material Design and Spring boot for backend.
+
+# Tecnologies:
+- Angular 6
+- Material Design
+- Spring Boot
+- Java 8
+- JPA/Hibernate
+- Node Server
+- Maven (dependencies)
+- Yarn
+
+------
+
+# Used IDES:
+- Eclipse Photon
+- VSCODE
+
+------
+
+# How to install:
+
+- Install MySql (MySql community 8.0.12.0) with port: 3306 / (user: root, password: admin)
+
+- Clone repositories:
+
+	- frontend: git clone https://github.com/brunoFreiberger/tasklist-frontend.git
+	- backend: git clone https://github.com/brunoFreiberger/tasklist-backend.git
+
+# How to run:
+
+# Frontend
+1. After clone, Run yarn install or npm install for install dependencies
+2. After you have downloaded dependencies, run yarn start or npm start
+
+# Backend
+1. After clone, Import Maven Project
+2. Maven -> Update Maven Project for download dependencies
+3. Run TasklistApplication.java
+
+------
+
+# Demo
+
+
+# Reponsivity
--- tasklist/src/main/java/br/com/supero/tasklist/models/Task.java
+++ tasklist/src/main/java/br/com/supero/tasklist/models/Task.java
@@ -15,59 +15,68 @@ import javax.persistence.Table;
  *
  */
 @Entity
-@Table(name="task")
+@Table(name = "task")
 public class Task {
-	
+
 	@Id
-    @GeneratedValue(strategy=GenerationType.AUTO)
+	@GeneratedValue(strategy = GenerationType.AUTO)
 	private Long id;
-	
+
 	@Column(name = "title")
 	private String title;
-	
+
 	@Column(name = "priority")
 	private Integer priority;
-	
+
 	@Column(name = "creation_date")
 	private Date creationDate;
-	
+
 	@Column(name = "order_task")
 	private Integer orderTask;
-	
+
 	@Column(name = "completed")
 	private Boolean completed;
-	
+
 	public Task() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getTitle() {
 		return title;
 	}
+
 	public void setTitle(String title) {
 		this.title = title;
 	}
+
 	public Date getCreationDate() {
 		return creationDate;
 	}
+
 	public void setCreationDate(Date creationDate) {
 		this.creationDate = creationDate;
 	}
+
 	public Integer getOrderTask() {
 		return orderTask;
 	}
+
 	public void setOrderTask(Integer orderTask) {
 		this.orderTask = orderTask;
 	}
+
 	public Boolean getCompleted() {
 		return completed;
 	}
+
 	public void setCompleted(Boolean completed) {
 		this.completed = completed;
 	}
@@ -79,5 +88,5 @@ public class Task {
 	public void setPriority(Integer priority) {
 		this.priority = priority;
 	}
-	
+
 }
